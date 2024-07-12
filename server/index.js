4fdd96f23dc8f46c1bded10dac4362ea24f3b9c9
vaibhavpandey00 <vaibhav.pandey0798@gmail.com>
2024-07-12 12:16:39 +0530

    Working on server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log("Server Running");
+import express from "express";
+// require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
