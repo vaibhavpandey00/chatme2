0a9715211b3bd0d38ff86c61b8b5c15aa6a7f965
vaibhavpandey00 <vaibhav.pandey0798@gmail.com>
2024-07-30 12:15:21 +0530

    Backend Completed With API's

--- server/index.js
+++ server/index.js
@@ -3,19 +3,29 @@ import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import { connDB } from "./utils/features.js";
-// import { createUser } from "./seeders/user.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { NEW_MESSAGE, NEW_MESSAGE_USER } from "./constants/events.js";
+import { v4 as uuid } from "uuid";
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/messageModel.js";
+// import { createUser } from "./seeders/user.js";
 
 dotenv.config();
 const MongoURI = process.env.MONGO_URI;
+const userSocketIDs = new Map();
 
 // Continue from 34500 Get Messages might face error
 // 50500 getMyFriends might face error
 // Skiped from 60000 Error handling
-// Continue from 54700
+// 62700 Starting work with Socket.io
+// 2nd Part Completed with Socket.io
 
 const app = express();
+const sever = createServer(app);
+const io = new Server(sever, {});
 
 // Using Middleware here to parse json and cookie parser
 app.use(express.json());
@@ -38,6 +48,64 @@ app.get("/", (req, res) => {
     res.send("Server is running");
 })
 
-app.listen(PORT, () => {
+io.use((socket, next) => { });
+
+io.on("connection", (socket) => {
+
+    const user = {
+        _id: "dfbdr",
+        name: "Jone Doe",
+    }
+
+    userSocketIDs.set(user._id.toString(), socket.id);
+
+    console.log(userSocketIDs);
+
+    socket.on(NEW_MESSAGE_USER, async ({ chatId, members, message }) => {
+
+        const messageFroRealTime = {
+            content: message,
+            _id: uuid(),
+            sender: {
+                _id: user._id,
+                name: user.name
+            },
+            chatId,
+            createdAt: new Date().toISOString()
+        }
+
+        const messageFroDB = {
+            content: message,
+            sender: user._id,
+            chatId
+        }
+
+        const membersSocket = getSockets(members);
+        io.to(membersSocket).emit(NEW_MESSAGE_USER, {
+            chatId,
+            message: messageFroRealTime
+        })
+        io.to(membersSocket).emit(NEW_MESSAGE, {
+            chatId
+        })
+
+        try {
+            await Message.create(messageFroDB);
+        } catch (error) {
+            console.log(error);
+        }
+
+        // console.log("New Message", messageFroRealTime);
+    })
+
+    socket.on("disconnect", () => {
+        userSocketIDs.delete(user._id.toString());
+        console.log("User Disconnected", socket.id);
+    })
+})
+
+sever.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
+
+export { userSocketIDs }
--- server/lib/helper.js
+++ server/lib/helper.js
@@ -1,3 +1,11 @@
+import { userSocketIDs } from "../index.js";
+
 export const getOtherMember = (members, userId) => {
     return members.find((member) => member._id !== userId);
+}
+
+export const getSockets = (users=[]) => {
+    const sockets = users.map((user) => userSocketIDs.get(user._id.toString()));
+
+    return sockets;
 }
